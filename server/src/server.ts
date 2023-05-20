8a367830234d5c7d0da7693104568b428bca97ee
Denis Pimentel Raineri <denis.raineri@nextpage.com.br>
2023-05-20 20:40:39 -0300

    first commit - aula 02

---
+++ server/src/server.ts
@@ -0,0 +1,18 @@
+import fastify from 'fastify';
+import { memoriesRoutes } from './routes/memories';
+import cors from '@fastify/cors';
+
+const app = fastify();
+
+app.register(cors, {
+  origin: true,
+});
+app.register(memoriesRoutes);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🤣 Server running atumalaca http://localhost:3333');
+  });

85ea7dc3040f588f9149a89ba1ab8a285b62a6d1
Denis Pimentel Raineri <denis.raineri@nextpage.com.br>
2023-05-21 22:55:45 -0300

    aula 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,25 @@
 import fastify from 'fastify';
-import { memoriesRoutes } from './routes/memories';
 import cors from '@fastify/cors';
+import jwt from '@fastify/jwt';
+import 'dotenv/config';
+import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const app = fastify();
 
 app.register(cors, {
   origin: true,
 });
+app.register(jwt, {
+  secret: 'spacetime',
+});
 app.register(memoriesRoutes);
+app.register(authRoutes);
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🤣 Server running atumalaca http://localhost:3333');
